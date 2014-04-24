46f681d8851733edf55e20f0b63f3eda5b4f3ee7
Shubhanshu <yadavshubhanshu@gmail.com>
2014-04-24 14:14:25 -0400

    Cleanup

--- app/views.py
+++ app/views.py
@@ -4,19 +4,27 @@ from app import app
 from flask import Flask,render_template, flash, redirect, request, url_for
 from forms import SearchForm
 
+
 @app.route('/',methods = ['GET', 'POST'])
 @app.route('/index',methods = ['GET', 'POST'])
 @app.route('/searchSeries', methods = ['GET', 'POST'])
 def searchSeries():
     form = SearchForm()
+
+    #Redirect to the same page after recieving request
     if form.validate_on_submit() and request.method=='POST':
     	#print request.method
+
     	flash('Series requested is : '+form.seriesName.data)
+
     	message = form.seriesName.data
         message =  helperFunctions.processSeriesString(message)
 
     	message[1] = message[1]+".json"
+
         showExists =  os.path.isfile(os.getcwd()+"/app/static/"+message[1])
+        
+        #if show file exists, then use that file otherwise use scrapy to generate the json and then use that
     	if showExists:
             return render_template("generateChart.html",showName = message[1])
         else:
