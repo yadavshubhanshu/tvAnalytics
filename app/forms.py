8ee53d02134839f459beae5a7c058a8a83821508
Shubhanshu <yadavshubhanshu@gmail.com>
2014-04-24 13:46:06 -0400

    Cleanup

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,6 @@ from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
-class LoginForm(Form):
+class SearchForm(Form):
 	seriesName = TextField('seriesName',validators = [Required()])
 		
