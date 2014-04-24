46746e2582ed1f645dac361f62a81d44555a0ffe
Shubhanshu <yadavshubhanshu@gmail.com>
2014-04-24 14:13:49 -0400

    Cleanup

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,8 @@ from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
+
+#Creating a basic Search Text Field
 class SearchForm(Form):
 	seriesName = TextField('seriesName',validators = [Required()])
 		
